5a98b2ade598cc23312ae40f16164104631a516e
Daniel <danielrobles1190@gmail.com>
2020-08-09 21:56:42 -0400

    Aumente los comentarios en las rutas.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layout');
 });
+
+
+/* RUTAS CAMILA STEFANIE YACOB*/
+
+
+/* RUTAS GERSON OLIVA*/
+
+
+/* RUTAS DANIEL ROBLES*/
