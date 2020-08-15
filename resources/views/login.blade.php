0e8dda35446fed41c935cb23f3b7d484c7b87aa4
Gerson Oliva <gerson_0007@hotmail.com>
2020-08-15 17:07:11 -0400

    no da el login auth de laravel ayudaaa

---
+++ resources/views/login.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>VABOL | L.T.D.A.</title>
+
+    <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="/datatables.net-bs/css/dataTables.bootstrap.min.css">
+    <!-- Data Tables -->
+    <link rel="stylesheet" href="/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+
+<body>
+
+
+    <div class="row  justify-content-center">
+
+        <div class="col-md-4 p-5">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h1 class="card-title">
+                        Acceso a la aplicacion
+                    </h1>
+                </div>
+                <div class="card-body">
+                    <form action="">
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input class="form-control" type="email" name="email" placeholder="Ingresa tu email">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Contraseña</label>
+                            <input class="form-control" type="password" name="password" placeholder="Ingresa tu email">
+                        </div>
+                    </form>
+                </div>
+
+                <div class="btn btn-primary btn-block">Ingresar</div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
