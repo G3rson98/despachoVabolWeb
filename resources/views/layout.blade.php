7e6a8964397db8066b1dc5b5c61127172412358e
CamCamCwi <camilayacobc@gmail.com>
2020-08-19 16:29:01 -0400

    Colores casi listo, solo falta agregar en la base el campo y hacer la consulta al iniciar sesion.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -25,11 +25,11 @@
 
 
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light" id="innerNavbar">
       <!-- Left navbar links -->
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+          <a class="nav-link" data-widget="pushmenu" href="#" id="menuIcon"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
           <a href="index3.html" class="nav-link">Home</a>
@@ -54,16 +54,16 @@
       <!-- Right navbar links -->
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-          <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i class="fas fa-cog"></i></a>
+          <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" id="ColorConfig"><i class="fas fa-cog"></i></a>
         </li>
       </ul>
     </nav>
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <aside class="main-sidebar sidebar-dark-primary elevation-4" id="mainSideBar">
       <!-- Brand Logo -->
-      <a href="index3.html" class="brand-link">
+      <a href="index3.html" class="brand-link" id="NavLogo">
         <img src="/dist/img/logoV.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">VABOL L.T.D.A. </span>
       </a>
@@ -77,7 +77,7 @@
           </div>
           <div class="info">
             @auth
-            <a href="#" class="d-block">
+            <a href="#" class="d-block" id="NavUser">
               <h>
                 {{auth()->user()->email}}
               </h>
@@ -93,7 +93,7 @@
                with font-awesome or any other icon font library -->
             
             <li class="nav-item has-treeview menu-open">
-              <a href="#" class="nav-link active">
+              <a href="#" class="nav-link active"  id="NavUsuarios">
                 <i class="nav-icon fas fa-user-alt"></i>
                 <p>
                   Usuarios
@@ -102,13 +102,13 @@
               </a>
               <ul class="nav nav-treeview">
                 <li class="nav-item">
-                  <a href="#" class="nav-link">
+                  <a href="#" class="nav-link" id="NavAbogado">
                     <i class="far fa-user-circle nav-icon"></i>
                     <p>Abogado</p>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="#" class="nav-link">
+                  <a href="#" class="nav-link" id="NavCliente">
                     <i class="far fa-user-circle nav-icon"></i>
                     <p>Cliente</p>
                   </a>
@@ -123,7 +123,7 @@
             </li>
             
             <li class="nav-item has-treeview menu-open">
-              <a href="{{ route('documento.create') }}" class="nav-link active">
+              <a href="{{ route('documento.create') }}" class="nav-link active" id="NavDoc">
                 <i class="nav-icon fas fa-file-alt"></i>
                 <p>
                   Documento
@@ -132,13 +132,13 @@
               </a>
               <ul class="nav nav-treeview">
                 <li class="nav-item">
-                  <a href="#" class="nav-link">
+                  <a href="#" class="nav-link" id="NavDocu">
                     <i class="far fa-file nav-icon"></i>
                     <p>Documentos</p>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="{{ route('categoriadocumento.index') }}" class="nav-link">
+                  <a href="{{ route('categoriadocumento.index') }}" class="nav-link" id="NavCdoc">
                     <i class="far fa-folder-open nav-icon"></i>
                     <p>Categorías Documento</p>
                   </a>
@@ -146,7 +146,7 @@
               </ul>
             </li>
             <li class="nav-item has-treeview menu-open">
-              <a href="#" class="nav-link active">
+              <a href="#" class="nav-link active" id="NavPub">
                 <i class="nav-icon fas fa-info"></i>
                 <p>
                   Publicaciones
@@ -155,19 +155,19 @@
               </a>
               <ul class="nav nav-treeview">
                 <li class="nav-item">
-                  <a href="{{ route('categoriaanuncio.index') }}" class="nav-link">
+                  <a href="{{ route('categoriaanuncio.index') }}" class="nav-link" id="NavCanu">
                     <i class="far fa-folder nav-icon"></i>
                     <p>Categoría Anuncios</p>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="{{ route('anuncio.index') }}" class="nav-link">
+                  <a href="{{ route('anuncio.index') }}" class="nav-link" id="NavAnu">
                     <i class="far fa-edit nav-icon"></i>
                     <p>Anuncios</p>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="{{ route('solicitudcontacto.index') }}" class="nav-link" style="color: cadetblue">
+                  <a href="{{ route('solicitudcontacto.index') }}" class="nav-link" id="NavSol">
                     <i class="far fa-address-card nav-icon"></i>
                     <p>Solicitudes de contacto</p>
                   </a>
@@ -202,9 +202,9 @@
             <div class="p-3">
               <h5>¿Qué tema quieres?</h5>
               <div class="d-flex flex-wrap mb-3">
-                <div class="bg-gray elevation-2" style="width: 40px; height: 20px; border-radius: 25px; margin-right: 10px; margin-bottom: 10px; opacity: 0.8; cursor: pointer;"></div>
-                <div class="bg-light elevation-2" style="width: 40px; height: 20px; border-radius: 25px; margin-right: 10px; margin-bottom: 10px; opacity: 0.8; cursor: pointer;"></div>
-                <div class="bg-danger elevation-2" style="width: 40px; height: 20px; border-radius: 25px; margin-right: 10px; margin-bottom: 10px; opacity: 0.8; cursor: pointer;"></div>
+                <div class="bg-gray elevation-2" onclick="Theme('#F8F9FA','#212529','#343A40');" style="width: 40px; height: 20px; border-radius: 25px; margin-right: 10px; margin-bottom: 10px; opacity: 0.8; cursor: pointer;"></div>
+                <div class="bg-light elevation-2" onclick="Theme('#2176ff','#ffffff','#eff1f3');" style="width: 40px; height: 20px; border-radius: 25px; margin-right: 10px; margin-bottom: 10px; opacity: 0.8; cursor: pointer;"></div>
+                <div class="bg-danger elevation-2" onclick="Theme('#F8F9FA','#bfc0c0','#D80032');" style="width: 40px; height: 20px; border-radius: 25px; margin-right: 10px; margin-bottom: 10px; opacity: 0.8; cursor: pointer;"></div>
               </div>
             </div>
           </aside>
@@ -270,6 +270,32 @@
         modal.find('.modal-body #doc_descripcion').val(doc_descripcion);
         modal.find('.modal-body #doc_id').val(doc_id);
       })
+
+      //CAM
+      function Theme(colorText,colorBG,colorNavbar){
+
+        document.getElementById("mainSideBar").style.background = colorNavbar;
+        document.getElementById("innerNavbar").style.background = colorBG;
+
+        document.getElementById("NavUsuarios").style.background = colorBG;
+        document.getElementById("NavDoc").style.background = colorBG;
+        document.getElementById("NavPub").style.background = colorBG;
+        
+        document.getElementById("NavUsuarios").style.color = colorText;
+        document.getElementById("NavAbogado").style.color = colorText;
+        document.getElementById("NavCliente").style.color = colorText;
+        document.getElementById("NavDoc").style.color = colorText;
+        document.getElementById("NavDocu").style.color = colorText;
+        document.getElementById("NavCdoc").style.color = colorText;
+        document.getElementById("NavPub").style.color = colorText;
+        document.getElementById("NavCanu").style.color = colorText;
+        document.getElementById("NavAnu").style.color = colorText;
+        document.getElementById("NavSol").style.color = colorText;
+        document.getElementById("NavUser").style.color = colorText;
+        document.getElementById("NavLogo").style.color = colorText;
+        document.getElementById("menuIcon").style.color = colorText;
+        document.getElementById("ColorConfig").style.color = colorText;
+      }
     </script>
   </div>
   </div>
