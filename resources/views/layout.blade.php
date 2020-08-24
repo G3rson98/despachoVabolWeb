fc0416bbc8eabf232a4fc2758876b0dc6e3623b2
Daniel <danielrobles1190@gmail.com>
2020-08-24 18:02:47 -0400

    Cambios

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -134,12 +134,14 @@
                 </p>
               </a>
               <ul class="nav nav-treeview">
+              @if(auth()->user()->rol == 'Administrador' || auth()->user()->rol == 'Abogado')
                 <li class="nav-item">
                   <a href="{{ route('documento.create') }}" class="nav-link" id="NavDocu">
                     <i class="far fa-file nav-icon"></i>
                     <p>Documentos</p>
                   </a>
                 </li>
+              @endif
                 <li class="nav-item">
                   <a href="{{ route('categoriadocumento.index') }}" class="nav-link" id="NavCdoc">
                     <i class="far fa-folder-open nav-icon"></i>
@@ -177,6 +179,7 @@
                 </li>
               </ul>
             </li>
+            @if(auth()->user()->rol == 'Administrador')
             <li class="nav-item has-treeview menu-open">
               <a href="#" class="nav-link active" id="NavDoc">
                 <i class="fas fa-chart-pie"></i>
@@ -194,6 +197,7 @@
                 </li>
               </ul>
             </li>
+            @endif
             <li class="nav-item">
               <a href="{{ route('bitacora') }}" class="nav-link" id="">
                 <button class="btn btn-primary btn-block"><i class="far fa-chart-bar nav-icon"></i> Bitácora</button>
